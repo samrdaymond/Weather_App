63f8e6b1f5e884bb99916f77d4d319c1a868a0d9
samrdaymond <106212725+samrdaymond@users.noreply.github.com>
2022-09-12 15:56:36 +1000

    finish need to test

--- modules/alb/alb.tf
+++ modules/alb/alb.tf
@@ -1,9 +1,7 @@
-#creation of alb next week
+#creation of security group for alb
 
-
-#create security group for alb
-resource "aws_security_group" "weather-app-alb-sg" {
-  description = "weather-app-alb-sg"
+resource "aws_security_group" "samrdaymond_wa_alb_sg" {
+  description = "weather app alb security group"
   vpc_id = aws_vpc.samrdaymond_wa_vpc.id
   ingress = [ {
     cidr_blocks = ["0.0.0.0/0"]
@@ -13,3 +11,34 @@ resource "aws_security_group" "weather-app-alb-sg" {
     protocol = "HTTP"    
   } ]
 }
+#creation of alb
+
+resource "aws_lb" "samrdaymond_wa_alb" {
+  name               = "weather-app-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = aws_security_group.samrdaymond_wa_alb_sg.id
+  subnets            = [aws_subnet.samrdaymond_wa_public_sub_a.id, aws_subnet.samrdaymond_wa_public_sub_b.id]
+  
+  enable_deletion_protection = false
+}
+ 
+resource "aws_alb_target_group" "samrdaymond_alb_tg" {
+  name        = "samrdaymond-weather-app-tg"
+  port        = 3000
+  protocol    = "HTTP"
+  vpc_id      = aws_vpc.samrdaymond_wa_vpc.id
+  target_type = "ip"
+
+
+resource "aws_alb_listener" "http" {
+  load_balancer_arn = aws_lb.samrdaymond_wa_alb.arn
+  port              = 80
+  protocol          = "HTTP"
+ 
+  default_action {
+   type = "forward"
+   target_group_arn = aws_alb_target_group.samrdaymond_alb_tg.arn
+  }
+}
+
--- modules/ecs/ecs.tf
+++ modules/ecs/ecs.tf
@@ -1,6 +1,3 @@
-#### creation of ecs to be completed next week
-
-
 # create security group for ecs
 
 resource "aws_security_group" "weather-app-ecs-sg" {
@@ -37,10 +34,77 @@ resource "aws_iam_role" "samrdaymond_ecs_ecrrole" {
 
 #create ecs execution role and policy
 
-resource "aws_iam_rol" "samrdaymondEcsExecutionRole" {
+resource "aws_iam_role" "samrdaymondEcsExecutionRole" {
   name = "samrdaymondEcsExecutionRole"
   assume_role_policy = jsonencode({
     
   }
   
+}
+
+
+resource "aws_ecs_cluster" "samrdaymond_wa_ecs_cluster" {
+  name = "weather-app-cluster"
+
+}
+#creation of ecs task definition
+
+resource "aws_ecs_task_definition" "samrdaymond_wa_ecs_td" {
+  container_definitions = jsonencode([
+    {
+    "family": "weather-app-fam",
+    "containerDefinitions": [
+        {
+            "name": "weather-app",
+            "image": "{ECR-repo-uri}",
+            "portMappings": [
+                {
+                    "protocol": "tcp",
+                    "containerPort": 3000
+                }
+            ],
+            "cpu": 0
+            }
+        }
+   ],
+    "memory": "512",
+    "cpu": "256",
+    "requiresCompatibilities": [
+        "FARGATE"
+    ],
+    "networkMode": "awsvpc",
+    "executionRoleArn": "{ecr-ecs-task-execution-role-arn}"
+}
+  ])
+
+  tags = {
+    Name        = "${var.name}-task-${var.environment}"
+    Environment = var.environment
+  }
+}
+
+#creation of ecs service
+
+resource "aws_ecs_service" "samrdaymond_wa_ecsservice" {
+  name                               = "weather-app-service"
+  cluster                            = aws_ecs_cluster.samrdaymond_wa_ecs_cluster.id
+  task_definition                    = aws_ecs_task_definition.samrdaymond_wa_ecs_td.arn
+  desired_count                      = 1
+  launch_type                        = "FARGATE"
+  iam_role                           = aws_iam_role.samrdaymondEcsExecutionRole.arn
+  
+
+
+  network_configuration {
+    security_groups  = var.ecs_service_security_groups
+    subnets          = "[aws_subnet.samrdaymond_wa_public_sub_a.id, aws_subnet.samrdaymond_wa_public_sub_b.id]"
+    assign_public_ip = false
+  }
+
+  load_balancer {
+    target_group_arn = var.aws_alb_target_group_arn
+    container_name   = "${var.name}-container-${var.environment}"
+    container_port   = var.container_port
+  }
+
 }
